e99ceca41953e7a124d37dd194ee1055fa54cac7
Manawariqbal <mmiqbalmanawar@gmail.com>
2021-09-14 16:07:11 +0530

    first

---
+++ src/config/fbconfig.js
@@ -0,0 +1,19 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBgZFYPER8w1bA28dd8-Vk1D-g5N9uVfHE",
+    authDomain: "evernote-clone-e45e9.firebaseapp.com",
+    projectId: "evernote-clone-e45e9",
+    storageBucket: "evernote-clone-e45e9.appspot.com",
+    messagingSenderId: "660026074626",
+    appId: "1:660026074626:web:70f99aa3df8fc109e2291b",
+    measurementId: "G-C2HZSZYHPE"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+// Initialize Firebase
+
+export default firebase;
